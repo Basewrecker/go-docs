25566b5c082166d08017cbf0bbb6a89f6ef0d995
Basewrecker <siddhunrs@gmail.com>
2026-05-24 17:59:14 +0530

    pointers via a wallet

--- shapes/shapes.go
+++ shapes/shapes.go
@@ -1,14 +1,33 @@
 package shapes
 
+import "math"
+
 type Rectangle struct {
 	width  float64
 	height float64
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.width + rectangle.height)
+type Shape interface {
+	Area() float64
 }
 
-func Area(rectangle Rectangle) float64 {
-	return rectangle.width * rectangle.height
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * (t.Base * t.Height)
 }
