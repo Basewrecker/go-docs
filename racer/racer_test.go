069df9cab78ac09aca4d4c64110196e4d8ab92fb
Basewrecker <siddhunrs@gmail.com>
2026-06-02 11:34:34 +0530

    refactor and server delayed + masked server

--- racer/racer_test.go
+++ racer/racer_test.go
@@ -1,15 +1,54 @@
+// You have been asked to make a function called WebsiteRacer which takes two URLs and "races" them by
+// hitting them with an HTTP GET and returning the URL which returned first. If none of them return
+// within 10 seconds then it should return an error
+
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	t.Run("Compare the speeds of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		defer slowServer.Close()
+		defer fastServer.Close()
 
+		slowURL := slowServer.URL
+		fastURL := fastServer.URL
+
+		want := fastURL
+		got, _ := Racer(slowURL, fastURL)
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	})
+
+	t.Run("Returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+			_, err := Racer(serverA.URL, serverB.URL)
+			if err == nil {
+				t.Error("Expected an error but didn't get one")
+			}
+	})
+}
+
+// Delayed server func
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		w.WriteHeader(http.StatusOK)
+	}))
 }
