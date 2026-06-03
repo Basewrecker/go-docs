7701729b0a22e8d5e801a95c5afd4ef4319ff7b3
Basewrecker <siddhunrs@gmail.com>
2026-06-03 12:50:10 +0530

    final fix and refactor

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -1,16 +1,41 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("Incrementing the counter 3 times leaves it at 3", func(t *testing.T) {
-		counter := Counter{}
+		counter := &Counter{}
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounter(t, counter, 3)
 	})
+
+	t.Run("It runs safe concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := &Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i = i + 1 {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+		assertCounter(t, counter, wantedCount)
+	})
+}
+
+func assertCounter(t testing.TB, got *Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d want %d", got.Value(), want)
+	}
 }
