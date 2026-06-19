978d631ac90ba5536287e8d30b3bd9f4388b6cf5
Basewrecker <siddhunrs@gmail.com>
2026-06-19 16:50:38 +0530

    generics testing

--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"g-test/blogrenderer"
 	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
 )
 
 func TestRender(t *testing.T) {
@@ -18,18 +20,12 @@ func TestRender(t *testing.T) {
 
 	t.Run("converts the single post into hTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
 
-		if err != nil {
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
-
 		got := buf.String()
-		want := `<h1>Hello world</h1>
-<p>This is a description</p>
-Tags: <ul><li>go</li><li>tdd</li></ul>`
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		approvals.VerifyString(t, got)
+
 	})
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module g-test
 
 go 1.21
+
+require github.com/approvals/go-approval-tests v1.11.0 // indirect
