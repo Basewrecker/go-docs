8ac271ed560445201e93c2d3c9ccab845b611373
Basewrecker <siddhunrs@gmail.com>
2026-07-28 10:45:41 +0530

    bug fix

--- server/CLI.go
+++ server/CLI.go
@@ -2,16 +2,21 @@ package poker
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
 	"strings"
 	"time"
 )
 
 var dummySpyAlerter = &SpyBlindAlerter{}
 
+const PlayerPrompt = "Please enter the amt of players"
+
 type CLI struct {
 	playerStore PlayerStore
-	in          io.Reader
+	in          *bufio.Scanner
+	out         io.Writer
 	alerter     BlindAlerter
 }
 
@@ -26,33 +31,40 @@ type BlindAlerter interface {
 	ScheduleAlertAt(duration time.Duration, amount int)
 }
 
-func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
+func NewCLI(store PlayerStore, in io.Reader, out io.Writer, alerter BlindAlerter) *CLI {
 	return &CLI{
 		playerStore: store,
 		in:          bufio.NewScanner(in),
+		out:         out,
 		alerter:     alerter,
 	}
 }
 
+func NewGame(alerter BlindAlerter, store PlayerStore) *Game {
+	return &Game{
+		alerter: alerter,
+		store:   store,
+	}
+}
 func (cli *CLI) PlayPoker() {
+	fmt.Fprint(cli.out, PlayerPrompt)
+	numberOfPlayers, _ := strconv.Atoi(cli.readLine())
+	cli.scheduleBlindAlerts(numberOfPlayers)
+	userInput := cli.readLine()
+	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func (cli *CLI) scheduleBlindAlerts(numberOfPlayers int) {
+	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
 
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
 		cli.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime = blindTime + 10*time.Minute
+		blindTime = blindTime + blindIncrement
 	}
-
-	userInput := cli.readLine()
-	cli.playerStore.RecordWin(extractWinner(userInput))
 }
+
 func extractWinner(userInput string) string {
 	return strings.Replace(userInput, "wins", "", 1)
-}
-
-func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.alerts = append(s.alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
 }
--- server/CLI_test.go
+++ server/CLI_test.go
@@ -8,6 +8,11 @@ import (
 	"time"
 )
 
+var dummyBlindAlerter = &SpyBlindAlerter{}
+var dummyPlayerStore = &poker.StubPlayerStore{}
+var dummyStdIn = &bytes.Buffer{}
+var dummyStdOut = &bytes.Buffer{}
+
 func TestCLI(t *testing.T) {
 	t.Run("record chris win from user input", func(t *testing.T) {
 		in := strings.NewReader("Chris wins\n")
